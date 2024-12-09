e0018d95d0d8000272b068a869febbf5b37e2fe3
maymon-gif <55115809+maymon-gif@users.noreply.github.com>
2024-12-09 22:04:07 +0630

    Add files via upload

---
+++ hide_unhide.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+    var x = document.getElementById("myDIV1");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function myFunction2() {
+    var x = document.getElementById("myDIV2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
